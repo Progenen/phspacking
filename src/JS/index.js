e0a498269d2e20251085d5ca8137626aea1b0b0a
Stanislav <stanislav_2010@icloud.com>
2024-09-11 10:52:59 +0500

    Push

--- src/JS/index.js
+++ src/JS/index.js
@@ -8,7 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const menu = document.querySelector(".header__col--menu");
     const header = document.querySelector("header");
     const main = document.querySelector("main");
-    
+    const anchor = document.querySelector(".anchor");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 800) {
+            anchor.classList.add("active");
+        }
+
+        if (window.scrollY < 800) {
+            anchor.classList.remove("active");
+
+        }
+    })
+
     main.style.paddingTop = header.offsetHeight + "px";
 
     if (window.innerWidth <= 992) {
@@ -36,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             breakpoints: {
                 1200: {
-                   slidesPerView: 4 
+                    slidesPerView: 4
                 },
                 992: {
                     slidesPerView: 3
@@ -52,175 +64,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (window.innerWidth >= 992 && document.querySelector(".section-slides")) {
 
-        const sectionSlides = document.querySelectorAll(".section-slide");
-        const sectionSlidesBtns = document.querySelectorAll(".section-slide__btn");
-        const tl = gsap.timeline();
-
-        tl.fromTo('.section-slide--1',
-            {
-                y: '0',
-            },
-            {
-                y: '0'
-            }
-        )
-
-        tl.fromTo('.section-slide--2',
-            {
-                y: '0',
-            },
-            {
-                y: '-100%',
-            },
-        );
-        tl.fromTo('.section-slide--3',
-            {
-                y: '0',
-            },
-            {
-                y: "-200%",
-            },
-            1
-        )
-        tl.fromTo('.section-slide--2 .section-slide__btn',
-            {
-                y: '0'
-            },
-            {
-                y: "-500%"
-            },
-            0.9
-        )
-        tl.fromTo('.section-slide--2 .section-slide__top',
-            {
-                y: '0'
-            },
-            {
-                y: "-70%"
-            },
-            0.9
-        );
-
-        tl.fromTo('.section-slide--4',
-            {
-                y: '0',
-            },
-            {
-                y: '-300%'
-            },
-            1.2
-        );
-        tl.fromTo('.section-slide--3 .section-slide__btn',
-            {
-                y: '0'
-            },
-            {
-                y: "-500%"
-            },
-            1.1
-        )
-        tl.fromTo('.section-slide--3 .section-slide__top',
-            {
-                y: '0'
-            },
-            {
-                y: "-70%"
-            },
-            1.1
-        );
-
-        tl.fromTo('.section-slide--5',
-            {
-                y: '0',
-            },
-            {
-                y: '-400%'
-            },
-            1.4
-        );
-        tl.fromTo('.section-slide--4 .section-slide__btn',
-            {
-                y: '0'
-            },
-            {
-                y: "-500%"
-            },
-            1.3
-        )
-        tl.fromTo('.section-slide--4 .section-slide__top',
-            {
-                y: '0'
-            },
-            {
-                y: "-70%"
-            },
-            1.3
-        )
-        tl.fromTo('.section-slide--6',
-            {
-                y: '0',
-            },
-            {
-                y: '-500%'
-            },
-            1.6
-        );
-        
-        tl.fromTo('.section-slide--5 .section-slide__btn',
-            {
-                y: '0'
-            },
-            {
-                y: "-500%"
-            },
-            1.5        )
-        tl.fromTo('.section-slide--5 .section-slide__top',
-            {
-                y: '0'
-            },
-            {
-                y: "-70%"
-            },
-            1.5        )
-
-
-        tl.fromTo('.section-slide--7',
-            {
-                y: '0',
-            },
-            {
-                y: '-600%'
-            },
-            2.6
-        );
-        tl.fromTo('.section-slide--6 .section-slide__btn',
-            {
-                y: '0'
-            },
-            {
-                y: "-500%"
-            },
-            2.4
-        )
-        tl.fromTo('.section-slide--6 .section-slide__top',
-            {
-                y: '0'
-            },
-            {
-                y: "-70%"
-            },
-            2.4
-        )
-
-        ScrollTrigger.create({
-            animation: tl,
-            trigger: '.section-slides__wrappper',
-            start: "top top", // Анимация начинается, когда элемент входит в вид на 80%
-            end: "bottom bottom", // Заканчивается, когда элемент выходит из вида на 20%
-            scrub: 10, // Связывает анимацию со скроллом, делает её более плавной
-            markers: true, // Включает маркеры для отладки (убрать в финальной версии)
-            pinSpacing: false,
-            pin: true,
+        gsap.utils.toArray(".section-slide").forEach((panel, i) => {
+            let scrollFirst = ScrollTrigger.create({
+                trigger: panel,
+                start: "top top",
+                pin: true,
+                pinSpacing: false,
+                grub: 10
+            });
+            gsap.to(`.section-slide--${i} .section__slide-btn`, {
+                y: -120,
+                ease: "none",
+                scrollTrigger: {
+                    trigger: `.section-slide--${i} .section__slide-btn`,
+                    containerAnimation: scrollFirst,
+                    start: "center 80%",
+                    end: "center 20%",
+                    scrub: true
+                }
+            });
         })
     }
 });
